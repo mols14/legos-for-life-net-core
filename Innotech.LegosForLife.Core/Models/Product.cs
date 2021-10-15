2e74b536dbff0cd9e84f05bacf41ea1ac02201d1
Lars Juul <ljb@easv.dk>
2021-10-15 08:33:02 +0200

    Adding First Unit test and business model

---
+++ Innotech.LegosForLife.Core/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace InnoTech.LegosForLife.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Innotech.legosForLife.Core.Test/Models/ProductTest.cs
@@ -0,0 +1,48 @@
+using InnoTech.LegosForLife.Core.Models;
+using Xunit;
+
+namespace InnoTech.LegosForLife.Core.Test.Models
+{
+    public class ProductTest
+    {
+        private readonly Product _product;
+        public ProductTest()
+        {
+            _product = new Product();
+        }
+        
+        [Fact]
+        public void Product_CanBeInitialized()
+        {
+            Assert.NotNull(_product);
+        }
+        
+        [Fact]
+        public void Product_Id_MustBeLong()
+        {
+            Assert.True(_product.Id is int);
+        }
+
+        [Fact]
+        public void Product_SetId_StoresId()
+        {
+            _product.Id = 1;
+            Assert.Equal(1, _product.Id);
+        }
+        
+        [Fact]
+        public void Product_UpdateId_StoresNewId()
+        {
+            _product.Id = 1;
+            _product.Id = 2;
+            Assert.Equal(2, _product.Id);
+        }
+
+        [Fact]
+        public void Product_SetName_StoreNameAsString()
+        {
+            _product.Name = "legoBrick";
+            Assert.Equal("legoBrick",  _product.Name);
+        }
+    }
+}
